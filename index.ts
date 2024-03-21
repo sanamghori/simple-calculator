f316853da1db608f011082c6a12c3cf0f1d5c75d
sanamghori <sanaghori022@gmail.com>
2024-03-22 03:13:03 +0500

    push my simple calculatorto github

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 #!/user/bin/env node
 import inquirer from "inquirer";
 let answer = await inquirer.prompt([
-  { message: "enter first Number", type: "number", name: "firstNumber" },
-  { message: "enter second Number", type: "number", name: "secondNumber" },
+  { message: "enter you first Number", type: "number", name: "firstNumber" },
+  { message: "enter you second Number", type: "number", name: "secondNumber" },
   {
     message: "select on of the operators to perform operations",
     type: "list",
@@ -16,3 +16,4 @@ if (answer.operator==="addition"){console.log (answer.firstNumber+answer.secondN
 else if (answer.operator==="subtraction"){console.log (answer.firstNumber-answer.secondNumber)}
 if (answer.operator==="multiplication"){console.log (answer.firstNumber*answer.secondNumber)}
 else if (answer.operator==="division"){console.log (answer.firstNumber/answer.secondNumber)}
+console.log("at the end")
